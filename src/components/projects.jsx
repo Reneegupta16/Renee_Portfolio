8c0b2f8c2e76dc26a114fc82b5c7afa1f06f07e7
Renee Gupta <154832697+Reneegupta16@users.noreply.github.com>
2024-07-27 19:32:37 +0530

    Update projects.jsx

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,52 +1,54 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
+export function Project() {
+    const navigate = useNavigate();
 
-export function Project(){
-    return(
+    return (
         <div>
             <div className="mb-24"></div>
 
+            <hr className='pt-10 border-t border-[#a8a4f4] opacity-35' />
 
-<hr className = 'pt-10 border-t border-[#a8a4f4]
- opacity-35  '></hr>
+            <h1 className='pt-14 pb-8 text-4xl font-bold bg-[#0d2438] text-white'>Projects Portfolio</h1>
 
+            <div className='bg-[#0d2438] text-white'>
+                <div className='grid grid-cols-3 gap-1 justify-items-center'>
+                    <Projects
+                        image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'}
+                        title={'Dental Website'}
+                        description={'Responsive Website'}
+                        link={'https://reneegupta16.github.io/RG-Dental-World/'}
+                    />
+                    <Projects
+                        image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'}
+                        title={'Google Health Platform'}
+                        description={'Web Application'}
+                    />
+                </div>
 
-
-<h1 className='pt-14 pb-8 text-4xl font-bold bg-[#0d2438] text-white'>Projects portfolio</h1>
-
-<div className='bg-[#0d2438] text-white'>
-  <div className='grid grid-cols-3 gap-1 justify-items-center'>
-    <Projects image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'} title={'Dental Website'} description={'Responsive Website'} />
-    <Projects image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'} title={'Google Health Platform'} description={'Web Application'} />
-    
-  </div>
-
-  <div className='flex justify-center items-center pt-32'>
-    <button
-      className='hover:underline h-8 justify-center items-center text-[#a8a4f4]
- bg-transparent border border-solid border-[#a8a4f4]
- rounded py-2 px-4 hover:bg-[#a8a4f4]
- hover:text-white hidden md:flex lg:flex-row'
-      onClick={() => navigate('/projects')}
-    >
-      More projects
-    </button>
-  </div>
-</div>
-
-
+                <div className='flex justify-center items-center pt-32'>
+                    <button
+                        className='hover:underline h-8 justify-center items-center text-[#a8a4f4] bg-transparent border border-solid border-[#a8a4f4] rounded py-2 px-4 hover:bg-[#a8a4f4] hover:text-white hidden md:flex lg:flex-row'
+                        onClick={() => navigate('/projects')}
+                    >
+                        More projects
+                    </button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-function Projects({ image, title, description }) {
+function Projects({ image, title, description, link }) {
     return (
-      <div>
-        <img src={image} alt="'hello' "className='rounded h-full w-52' />
-        <div className='w-52 bg-[#3f5575]'>
-          <h1 className='text-1xl font-bold'>{title}</h1>
-          <h2 className='text-1xl'>{description}</h2>
+        <div>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <img src={image} alt={title} className='rounded h-full w-52' />
+            </a>
+            <div className='w-52 bg-[#3f5575]'>
+                <h1 className='text-1xl font-bold'>{title}</h1>
+                <h2 className='text-1xl'>{description}</h2>
+            </div>
         </div>
-      </div>
     );
-  }
+}
