d3513f64b624865c5c1c44c965e959203659de7d
Renee Gupta <154832697+Reneegupta16@users.noreply.github.com>
2024-07-27 19:17:44 +0530

    Update About-me.jsx

--- src/components/About-me.jsx
+++ src/components/About-me.jsx
@@ -4,13 +4,13 @@ export function About() {
         <img className="h-48 pl-28 pr-10 rounded" alt="" src="/profile.jpeg" />
         <p className="text-gray-200 text-left  ">Hello! I'm Renée Gupta, a pre-final year B.Tech student at Delhi Technological University majoring in Electronics Engineering with Minors in COmuter Science Engineeribg with a strong foundation in Java programming and Data Structures and Algorithms (DSA). Currently, I am enhancing my skills in Spring Boot to build robust and scalable backend applications and advancing my skills to develop and maintain scalable RESTful APIs. With a strong analytical mindset, I excel at solving complex problems and creating efficient software solutions.
 
-Academic Background
+<br></br>Academic Background
 I have had the opportunity to delve into various fascinating subjects, including Object Oriented Programming, Data Base Management System [SQL], Operating Systems [Linux], and Fundamental programming in C and MATLAB. My coursework has provided me with a broad interdisciplinary knowledge base, enabling me to approach problems from multiple angles and find innovative solutions.
 
-Professional Experience
+<br></br>Professional Experience
 My professional journey includes a Software Developer Internship at Pheuture Studio Pvt. Ltd., where I honed my skills in Core Java and Microservices. I also worked as a Software Developer Intern at Headstarter AI, focusing on Java and Data Structures and Algorithms (DSA). Additionally, I have a solid foundation in Python, Cloud Computing and Pipeline management along with fundamental knowledge of Machine Learning.
 
-Personal and Academic Achievements
+<br></br>Personal and Academic Achievements
 WISE’24 Scholar, TalentSprint 
 HeadStarter SWE Fellowship’24 
 Semi-Finalist of Coderush 4.0
