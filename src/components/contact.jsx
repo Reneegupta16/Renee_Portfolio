09e3ca320e74a1fee689dc5b7459ec397d9d4745
Renee Gupta <154832697+Reneegupta16@users.noreply.github.com>
2024-07-27 23:59:46 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -27,27 +27,25 @@ export function Contactpage() {
         inputName,
         placeholderText,
         ariaLabelName,
-    }) => {
-        return (
-            <div className="font-general-regular mb-4 text-white">
-                <label
-                    className="block text-lg text-primary-dark dark:text-primary-light mb-1"
-                    htmlFor={labelFor}
-                >
-                    {inputLabel}
-                </label>
-                <input
-                    className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-white dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
-                    type={inputType}
-                    id={inputId}
-                    name={inputName}
-                    placeholder={placeholderText}
-                    aria-label={ariaLabelName}
-                    required
-                />
-            </div>
-        );
-    };
+    }) => (
+        <div className="font-general-regular mb-4 text-white">
+            <label
+                className="block text-lg text-primary-dark dark:text-primary-light mb-1"
+                htmlFor={labelFor}
+            >
+                {inputLabel}
+            </label>
+            <input
+                className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-white dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
+                type={inputType}
+                id={inputId}
+                name={inputName}
+                placeholder={placeholderText}
+                aria-label={ariaLabelName}
+                required
+            />
+        </div>
+    );
 
     return (
         <div className="container mx-auto flex flex-col-reverse lg:flex-row py-5 lg:py-10">
@@ -77,6 +75,7 @@ export function Contactpage() {
                         onSubmit={(e) => {
                             e.preventDefault();
                             // Add form submission logic here
+                            alert('Form submitted');
                         }}
                         className="max-w-xl m-4 p-6 sm:p-10 bg-[#3f5575] dark:bg-secondary-dark rounded-xl shadow-xl text-left"
                     >
@@ -125,6 +124,7 @@ export function Contactpage() {
                                 cols="14"
                                 rows="6"
                                 aria-label="Message"
+                                required
                             ></textarea>
                         </div>
 
@@ -141,4 +141,3 @@ export function Contactpage() {
         </div>
     );
 }
-
