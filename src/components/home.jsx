8aef6a07ec0e511097bb7e26e4b4878d1f2614b4
Renee Gupta <154832697+Reneegupta16@users.noreply.github.com>
2024-07-28 02:32:31 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,9 @@
-import Pdf from '/ReneeGupta_Resume.pdf';
-import { useNavigate } from 'react-router-dom'
-import { motion } from "framer-motion"
-import { FaJava } from "react-icons/fa";
+
+  import Pdf from '/ReneeGupta_Resume.pdf';
+import { useNavigate } from 'react-router-dom';
+import { motion } from "framer-motion";
+import { FaJava, FaCss3Alt, FaPython, FaRobot } from "react-icons/fa";
 import { TbBrandLeetcode } from "react-icons/tb";
-import { FaCss3Alt} from "react-icons/fa";
-import { FaPython } from "react-icons/fa";
-import { FaRobot } from "react-icons/fa";
-
-
-
-
-
 
 import React, { useEffect, useState } from "react";
 import { useMotionValue, useTransform, animate } from "framer-motion";
@@ -45,149 +38,137 @@ const AnimatedText = ({ text }) => {
 
 export default AnimatedText;
 
-
 export function Home() {
-    const navigate = useNavigate();
-  
-    return (
-      <div className='font-roboto'>
-        <div>
-          
-          <div className='flex justify-between items-center bg-[#0d2438] text-white pb-8 pt-8 '>
+  const navigate = useNavigate();
 
-            <div className='text-left w-full ml-32'>
-            <motion.div
-  initial={{ opacity: 0, y: -50 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 1 }}
->
-              <h1 className='text-5xl font-bold text-left pb-4'>Hi, I am <span className='text-[#a8a4f4]
-'>Renée.</span></h1>
-              </motion.div>
-
-           
-              <h1 className='text-3xl font-bold text-left pb-8 pt-2'>
-                <AnimatedText text="Software and Full Stack Developer."/>
-              </h1>
-              
-              
-
-
-
-  
-              <button
-                className='hover:underline text-[#a8a4f4]
- bg-transparent border border-solid border-[#a8a4f4]
- rounded py-2 px-4 hover:bg-[#a8a4f4]
- hover:text-white'
-                onClick={() => window.open(Pdf, '_blank')}
-              >
-                Download Résume
-              </button>
-            </div>
-
-  
-            <img src='/landingpage.png' alt="'Profile picture'" className='h-[50vh] rounded-md pr-24 hidden md:flex' />
-            <div className='h-8'></div>
-          </div>
-
-
-          
-          <div className='grid grid-cols-7 justify-items-center ml-10  text-white h-32 subpixel-antialiased pt-8 '>
-            <div></div>
-            <div className='relative w-16 h-20 group'>
-              <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-              <FaJava alt="" className='absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0 
-' /> 
-              </motion.image>
-              <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
-                Java & Spring Boot
-              </div>
-            </div>
-
-            <div className='relative w-16 h-20 group'>
-            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-              <TbBrandLeetcode alt="" className='text-[#a8a4f4]
- absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' /> 
-              </motion.image>
-              <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
-                Data Structures & Algorithms
-              </div>
-            </div>
-
-            <div className='relative w-16 h-20 group'>
-            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-              <FaCss3Alt alt="" className='
- absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' /> 
-              </motion.image>
-              <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
-                Web Development [HTML, CSS, JS]
-              </div>
-            </div>
-
-            <div className='relative w-16 h-20 group'>
-            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-              <FaPython alt="" className='text-[#a8a4f4]
- absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' /> 
-              </motion.image>
-              <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
-                Python & Machine Learning
-              </div>
-            </div>
-
-            <div className='relative w-16 h-20 group'>
-            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-              <FaRobot alt="" className='
- absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' /> 
-              </motion.image>
-              <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
-                Particle Physics & AI
-              </div>
-              <div></div>
-            </div>
-
-          </div>
-          <div className="mb-40"></div>
-
-
-          <hr className = 'pt-10 border-t border-[#a8a4f4]
- opacity-35  '></hr>
-
-  
-          <h1 className='pt-14 pb-8 text-4xl font-bold bg-[#0d2438] text-white'>Projects portfolio</h1>
-  
-          <div className='bg-[#0d2438] text-white'>
-            <div className='grid grid-cols-3 gap-1 justify-items-center'>
-              <Projects image={'/dist/landingpage.png'} title={'Portfolio'} description={'Self Website'} />
-              <Projects image={'/dental.png'} title={'Responsive Dental Website'} description={'RG Denatl World'} />
-              <Projects image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'} title={'Google Health Platform'} description={'Web Application'} />
-            </div>
-  
-            <div className='flex justify-center items-center pt-32'>
-              <button
-                className='hover:underline h-8 justify-center items-center text-[#a8a4f4]
- bg-transparent border border-solid border-[#a8a4f4]
- rounded py-2 px-4 hover:bg-[#a8a4f4]
- hover:text-white hidden md:flex lg:flex-row'
-                onClick={() => navigate('/projects')}
-              >
-                More projects
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  
-  function Projects({ image, title, description }) {
-    return (
+  return (
+    <div className='font-roboto'>
       <div>
-        <img src={image} alt="'hello' "className='rounded h-full w-52' />
-        <div className='w-52 bg-[#3f5575]'>
-          <h1 className='text-1xl font-bold'>{title}</h1>
-          <h2 className='text-1xl'>{description}</h2>
+        <div className='flex justify-between items-center bg-[#0d2438] text-white pb-8 pt-8 '>
+          <div className='text-left w-full ml-32'>
+            <motion.div
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <h1 className='text-5xl font-bold text-left pb-4'>Hi, I am <span className='text-[#a8a4f4]'>Renée.</span></h1>
+            </motion.div>
+
+            <h1 className='text-3xl font-bold text-left pb-8 pt-2'>
+              <AnimatedText text="Software and Full Stack Developer." />
+            </h1>
+
+            <button
+              className='hover:underline text-[#a8a4f4] bg-transparent border border-solid border-[#a8a4f4] rounded py-2 px-4 hover:bg-[#a8a4f4] hover:text-white'
+              onClick={() => window.open(Pdf, '_blank')}
+            >
+              Download Résume
+            </button>
+          </div>
+
+          <img src='/landingpage.png' alt="'Profile picture'" className='h-[50vh] rounded-md pr-24 hidden md:flex' />
+          <div className='h-8'></div>
+        </div>
+
+        <div className='grid grid-cols-7 justify-items-center ml-10 text-white h-32 subpixel-antialiased pt-8 '>
+          <div></div>
+          <div className='relative w-16 h-20 group'>
+            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+              <FaJava alt="" className='absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' />
+            </motion.image>
+            <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
+              Java & Spring Boot
+            </div>
+          </div>
+
+          <div className='relative w-16 h-20 group'>
+            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+              <TbBrandLeetcode alt="" className='text-[#a8a4f4] absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' />
+            </motion.image>
+            <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
+              Data Structures & Algorithms
+            </div>
+          </div>
+
+          <div className='relative w-16 h-20 group'>
+            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+              <FaCss3Alt alt="" className='absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' />
+            </motion.image>
+            <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
+              Web Development [HTML, CSS, JS]
+            </div>
+          </div>
+
+          <div className='relative w-16 h-20 group'>
+            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+              <FaPython alt="" className='text-[#a8a4f4] absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' />
+            </motion.image>
+            <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
+              Python & Machine Learning
+            </div>
+          </div>
+
+          <div className='relative w-16 h-20 group'>
+            <motion.image initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+              <FaRobot alt="" className='absolute inset-0 size-5 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0' />
+            </motion.image>
+            <div className='absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100'>
+              Particle Physics & AI
+            </div>
+            <div></div>
+          </div>
+        </div>
+        <div className="mb-40"></div>
+
+        <hr className='pt-10 border-t border-[#a8a4f4] opacity-35'></hr>
+
+        <h1 className='pt-14 pb-8 text-4xl font-bold bg-[#0d2438] text-white'>Projects portfolio</h1>
+
+        <div className='bg-[#0d2438] text-white'>
+          <div className='grid grid-cols-3 gap-1 justify-items-center'>
+            <Projects 
+              image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'} 
+              title={'Portfolio'} 
+              description={'Self Website'} 
+              link={'https://66a55efa73d5d700085ba1d3--reneeguptaportfolio.netlify.app'}
+            />
+            <Projects 
+              image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'} 
+              title={'Responsive Dental Website'} 
+              description={'RG Dental World'} 
+              link={'https://reneegupta16.github.io/RG-Dental-World/'}
+            />
+            <Projects 
+              image={'https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg'} 
+              title={'Google Health Platform'} 
+              description={'Web Application'} 
+              link={'#'} // Add the live link of this project here
+            />
+          </div>
+
+          <div className='flex justify-center items-center pt-32'>
+            <button
+              className='hover:underline h-8 justify-center items-center text-[#a8a4f4] bg-transparent border border-solid border-[#a8a4f4] rounded py-2 px-4 hover:bg-[#a8a4f4] hover:text-white hidden md:flex lg:flex-row'
+              onClick={() => navigate('/projects')}
+            >
+              More projects
+            </button>
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+function Projects({ image, title, description, link }) {
+  return (
+    <div onClick={() => window.open(link, '_blank')} className='cursor-pointer'>
+      <img src={image} alt="'hello'" className='rounded h-full w-52' />
+      <div className='w-52 bg-[#3f5575]'>
+        <h1 className='text-1xl font-bold'>{title}</h1>
+        <h2 className='text-1xl'>{description}</h2>
+      </div>
+    </div>
+  );
+}
+
